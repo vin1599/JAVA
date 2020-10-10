91872a241600b0ea30d3b0bc685da37a7b9d389e
vin1599 <53434367+vin1599@users.noreply.github.com>
2020-10-10 17:37:19 +0530

    Create Employee.java

---
+++ Day2/Employee.java
@@ -0,0 +1,25 @@
+package com.employee;
+
+public class Employee {
+
+    String name = "Saurabh";
+    int age = 23;
+    String city = "Chennai";
+
+    //String name = new String(original:"Jaggu");
+    public void doSomething() {
+        System.out.println("The name is " + name);
+        System.out.println("The age is " + age);
+        System.out.println("The city is " + city);
+    }
+        String name1 = "Jagannath";
+        int age1 = 19;
+        String city1 = "Odisha";
+        public void display() {
+            System.out.println("The name is " + name1);
+            System.out.println("The age is " + age1);
+            System.out.println("The city is " + city1);
+
+        }
+
+    }
